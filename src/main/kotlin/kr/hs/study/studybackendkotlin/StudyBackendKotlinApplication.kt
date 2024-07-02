2e6878929ec635cf8da02cc071b43f8fef3f6e3a
MsEmily1020 <peace10200khs@gmail.com>
2024-07-02 12:12:19 +0900

    init: 프로젝트 초기 셋팅

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/StudyBackendKotlinApplication.kt
@@ -0,0 +1,11 @@
+package kr.hs.study.studybackendkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyBackendKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyBackendKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/kr/hs/study/studybackendkotlin/StudyBackendKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.hs.study.studybackendkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyBackendKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
