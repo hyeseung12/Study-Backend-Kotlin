e3a30d873b32be8ad1e76cf15e40dacca81a9405
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 15:46:12 +0900

    add: TransactionalService 커스텀 어노테이션 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/annotation/TransactionalService.kt
@@ -0,0 +1,13 @@
+package kr.hs.study.studybackendkotlin.annotation
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import kotlin.reflect.KClass
+
+@Service
+@Transactional(rollbackFor = [Exception::class])
+annotation class TransactionalService(
+    @get: AliasFor(annotation = Transactional::class)
+    val noRollbackFor: Array<KClass<out Throwable>> = []
+)
