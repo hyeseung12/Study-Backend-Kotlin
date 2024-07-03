cfeb7d4fe8e06c1b4e0ffdc552b7b3cfe42bcd9e
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 15:12:45 +0900

    add: user entity 추가

--- src/main/kotlin/kr/hs/study/studybackendkotlin/entity/User.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/entity/User.kt
@@ -1,9 +1,18 @@
 package kr.hs.study.studybackendkotlin.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 
 @Entity
 class User(
+    var name: String,
 
-) : BaseEntity() {
-}
+    @Column(unique = true)
+    val userId: String,
+
+    @Column(unique = true)
+    val email: String,
+
+    var password: String,
+
+) : BaseEntity()
