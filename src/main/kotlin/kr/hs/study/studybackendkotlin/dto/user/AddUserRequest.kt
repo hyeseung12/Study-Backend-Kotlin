ecb21e8e17830fb54f0e82b228277bb9011a5d35
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 15:29:55 +0900

    add: AddUserRequest 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/dto/user/AddUserRequest.kt
@@ -0,0 +1,29 @@
+package kr.hs.study.studybackendkotlin.dto.user
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import kr.hs.study.studybackendkotlin.entity.User
+
+data class AddUserRequest(
+    // field: 를 사용한 이유 : https://devlog-wjdrbs96.tistory.com/407
+    @field:NotNull
+    val name: String,
+
+    @field:NotBlank
+    val userId: String,
+
+    @field:NotBlank
+    val email: String,
+
+    @field:NotBlank
+    val password: String
+) {
+
+    fun toEntity() = User(
+        name = name,
+        userId = userId,
+        email = email,
+        password = password
+    )
+
+}
