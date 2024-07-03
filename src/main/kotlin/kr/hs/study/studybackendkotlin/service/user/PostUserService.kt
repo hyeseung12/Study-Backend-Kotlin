9801b18bae6d9e9b17576f8f7b198858425bac9e
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 15:55:28 +0900

    add: PostUserService 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/service/user/PostUserService.kt
@@ -0,0 +1,20 @@
+package kr.hs.study.studybackendkotlin.service.user
+
+import kr.hs.study.studybackendkotlin.annotation.TransactionalService
+import kr.hs.study.studybackendkotlin.dto.user.AddUserRequest
+import kr.hs.study.studybackendkotlin.repository.user.UserRepository
+
+@TransactionalService
+class PostUserService(
+    private val userRepository: UserRepository
+) {
+    fun execute(request: AddUserRequest) {
+        duplicateUserId(request.userId)
+        duplicateEmail(request.email)
+
+        userRepository.save(request.toEntity())
+    }
+
+    fun duplicateEmail(email: String) : Boolean = if(userRepository.existsByEmail(email)) throw RuntimeException("이메일 에러") else true
+    fun duplicateUserId(userId: String) : Boolean = if(userRepository.existsByUserId(userId)) throw RuntimeException("유저 아이디 에러") else true
+}
