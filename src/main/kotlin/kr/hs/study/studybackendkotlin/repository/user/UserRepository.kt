e8e2c5f1e7ef58a7b153042b64d697e3a2604c64
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 15:42:30 +0900

    add: UserRepository 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/repository/user/UserRepository.kt
@@ -0,0 +1,9 @@
+package kr.hs.study.studybackendkotlin.repository.user
+
+import kr.hs.study.studybackendkotlin.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+    fun existsByUserId(userId: String) : Boolean
+    fun existsByEmail(email: String): Boolean
+}
