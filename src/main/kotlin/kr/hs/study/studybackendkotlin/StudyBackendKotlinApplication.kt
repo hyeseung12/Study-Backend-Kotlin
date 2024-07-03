7179554ddd9ea87c902e60036f94f72d07e0d83f
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 14:48:47 +0900

    add: BaseEntity 추가

--- src/main/kotlin/kr/hs/study/studybackendkotlin/StudyBackendKotlinApplication.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/StudyBackendKotlinApplication.kt
@@ -2,8 +2,10 @@ package kr.hs.study.studybackendkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class StudyBackendKotlinApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/entity/BaseEntity.kt
@@ -0,0 +1,27 @@
+package kr.hs.study.studybackendkotlin.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+open class BaseEntity (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdDate: LocalDateTime? = null,
+
+    @LastModifiedDate
+    var lastModifiedBy: LocalDateTime? = null
+)
