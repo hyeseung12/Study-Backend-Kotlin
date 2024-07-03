ccc7bc71728fd4911c49fef2f727e2ea6a379746
MsEmily1020 <peace10200khs@gmail.com>
2024-07-03 15:47:01 +0900

    add: ReadOnlyService 커스텀 어노테이션 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/annotation/ReadOnlyService.kt
@@ -0,0 +1,13 @@
+package kr.hs.study.studybackendkotlin.annotation
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import kotlin.reflect.KClass
+
+@Service
+@Transactional(readOnly = true, rollbackFor = [Exception::class])
+annotation class ReadOnlyService(
+    @get: AliasFor(annotation = Transactional::class)
+    val noRollbackFor: Array<KClass<out Throwable>> = []
+)
