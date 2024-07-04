f258ba5a47d873deb9453afee377bf19384754dd
MsEmily1020 <peace10200khs@gmail.com>
2024-07-04 15:56:16 +0900

    feat: UpdateUserService 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/service/user/UpdateUserService.kt
@@ -0,0 +1,20 @@
+package kr.hs.study.studybackendkotlin.service.user
+
+import kr.hs.study.studybackendkotlin.annotation.TransactionalService
+import kr.hs.study.studybackendkotlin.dto.user.UpdateUserRequest
+import kr.hs.study.studybackendkotlin.dto.user.UserResponse
+import kr.hs.study.studybackendkotlin.repository.user.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+
+@TransactionalService
+class UpdateUserService(
+    private val userRepository: UserRepository
+) {
+    fun execute(id: Long, request: UpdateUserRequest): UserResponse {
+        val user = userRepository.findByIdOrNull(id)
+            ?: throw RuntimeException("user not found")
+
+        user.password = request.password
+        return UserResponse(user)
+    }
+}
