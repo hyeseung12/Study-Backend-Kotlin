258f453f19eb041a84abde4467a520275ca1d91e
MsEmily1020 <peace10200khs@gmail.com>
2024-07-04 15:59:08 +0900

    feat: UserController - updateUser 추가

--- src/main/kotlin/kr/hs/study/studybackendkotlin/controller/UserController.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/controller/UserController.kt
@@ -3,16 +3,15 @@ package kr.hs.study.studybackendkotlin.controller
 import jakarta.validation.Valid
 import kr.hs.study.studybackendkotlin.dto.user.AddUserRequest
 import kr.hs.study.studybackendkotlin.dto.user.LoginUserRequest
+import kr.hs.study.studybackendkotlin.dto.user.UpdateUserRequest
 import kr.hs.study.studybackendkotlin.dto.user.UserResponse
-import kr.hs.study.studybackendkotlin.service.user.DeleteUserService
-import kr.hs.study.studybackendkotlin.service.user.GetUserService
-import kr.hs.study.studybackendkotlin.service.user.LoginUserService
-import kr.hs.study.studybackendkotlin.service.user.PostUserService
+import kr.hs.study.studybackendkotlin.service.user.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -23,7 +22,8 @@ class UserController(
     private val postUserService: PostUserService,
     private val loginUserService: LoginUserService,
     private val getUserService: GetUserService,
-    private val deleteUserService: DeleteUserService
+    private val updateUserService: UpdateUserService,
+    private val deleteUserService: DeleteUserService,
 ) {
     @PostMapping
     fun addUser(@Valid @RequestBody request: AddUserRequest): ResponseEntity<Void> {
@@ -40,6 +40,12 @@ class UserController(
     @GetMapping("/{id}")
     fun getUser(@PathVariable id: Long): ResponseEntity<UserResponse> {
         val user = getUserService.execute(id)
+        return ResponseEntity.ok(user)
+    }
+
+    @PutMapping("/{id}")
+    fun updateUser(@PathVariable id:Long, @Valid @RequestBody request: UpdateUserRequest): ResponseEntity<UserResponse> {
+        val user = updateUserService.execute(id, request)
         return ResponseEntity.ok(user)
     }
 
