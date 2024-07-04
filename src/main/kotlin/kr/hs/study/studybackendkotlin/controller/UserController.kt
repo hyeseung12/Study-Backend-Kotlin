09f3d892aafa8f4966f79a502c2b46e1b00daa03
MsEmily1020 <peace10200khs@gmail.com>
2024-07-04 15:17:42 +0900

    feat: UserController - getUser 추가

--- src/main/kotlin/kr/hs/study/studybackendkotlin/controller/UserController.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/controller/UserController.kt
@@ -4,9 +4,12 @@ import jakarta.validation.Valid
 import kr.hs.study.studybackendkotlin.dto.user.AddUserRequest
 import kr.hs.study.studybackendkotlin.dto.user.LoginUserRequest
 import kr.hs.study.studybackendkotlin.dto.user.UserResponse
+import kr.hs.study.studybackendkotlin.service.user.GetUserService
 import kr.hs.study.studybackendkotlin.service.user.LoginUserService
 import kr.hs.study.studybackendkotlin.service.user.PostUserService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +19,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/user")
 class UserController(
     private val postUserService: PostUserService,
-    private val loginUserService: LoginUserService
+    private val loginUserService: LoginUserService,
+    private val getUserService: GetUserService
 ) {
     @PostMapping
     fun addUser(@Valid @RequestBody request: AddUserRequest): ResponseEntity<Void> {
@@ -27,6 +31,12 @@ class UserController(
     @PostMapping("/login")
     fun loginUser(@Valid @RequestBody request: LoginUserRequest): ResponseEntity<UserResponse> {
         val user = loginUserService.execute(request)
+        return ResponseEntity.ok(user)
+    }
+
+    @GetMapping("/{id}")
+    fun getUser(@PathVariable id: Long): ResponseEntity<UserResponse> {
+        val user = getUserService.execute(id)
         return ResponseEntity.ok(user)
     }
 }
