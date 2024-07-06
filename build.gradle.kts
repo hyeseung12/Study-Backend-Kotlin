ea02522b6ce8152626af2fcf5408ee6bd089c4cf
MsEmily1020 <peace10200khs@gmail.com>
2024-07-06 09:49:05 +0900

    add: database - postgresql 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,9 @@ dependencies {
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     kapt("jakarta.annotation:jakarta.annotation-api")
     kapt("jakarta.persistence:jakarta.persistence-api")
+
+    // database - postgresql
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 kotlin {
