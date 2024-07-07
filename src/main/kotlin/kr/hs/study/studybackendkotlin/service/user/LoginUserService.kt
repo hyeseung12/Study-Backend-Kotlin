a6ae971730f2aa7e4b789f32b6a5fa5ed4e78c25
MsEmily1020 <peace10200khs@gmail.com>
2024-07-07 11:46:02 +0900

    feat: LoginUserService - PasswordEncoder 추가

--- src/main/kotlin/kr/hs/study/studybackendkotlin/service/user/LoginUserService.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/service/user/LoginUserService.kt
@@ -4,13 +4,19 @@ import kr.hs.study.studybackendkotlin.annotation.TransactionalService
 import kr.hs.study.studybackendkotlin.dto.user.LoginUserRequest
 import kr.hs.study.studybackendkotlin.dto.user.UserResponse
 import kr.hs.study.studybackendkotlin.repository.user.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @TransactionalService
 class LoginUserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
     fun execute(request: LoginUserRequest): UserResponse {
         val user = userRepository.findByUserIdOrEmail(request.userId, request.password)
+
+        if(!passwordEncoder.matches(request.password, user.password))
+            throw RuntimeException("password is not equal")
+
         return UserResponse(user)
     }
 }
