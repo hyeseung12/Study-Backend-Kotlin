f5e0ef06fa4693afcca602105122edd6961eb0df
MsEmily1020 <peace10200khs@gmail.com>
2024-07-21 20:55:50 +0900

    add: jwt dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     // database - redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
+    // jwt
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 kotlin {
