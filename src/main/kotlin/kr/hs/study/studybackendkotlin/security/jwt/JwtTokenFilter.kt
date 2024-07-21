eef3a5c760a467cba32e7a8dd820856f5406de05
MsEmily1020 <peace10200khs@gmail.com>
2024-07-21 21:25:46 +0900

    feat: JwtTokenFilter, JwtTokenProvider 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/security/jwt/JwtTokenFilter.kt
@@ -0,0 +1,25 @@
+package kr.hs.study.studybackendkotlin.security.jwt
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+
+class JwtTokenFilter (
+    private val jwtTokenProvider: JwtTokenProvider
+): OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token: String? = jwtTokenProvider.resolveToken(request) // 1. 토큰 디코딩
+        token.let {
+            val authentication: Authentication = jwtTokenProvider.authorization(token!!)
+            SecurityContextHolder.getContext().authentication = authentication // 2. 토큰 사용자 권한 줌
+        }
+        filterChain.doFilter(request, response)
+    }
+}
---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/security/jwt/JwtTokenProvider.kt
@@ -0,0 +1,83 @@
+package kr.hs.study.studybackendkotlin.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import jakarta.servlet.http.HttpServletRequest
+import kr.hs.study.studybackendkotlin.config.properties.JwtProperties
+import kr.hs.study.studybackendkotlin.entity.RefreshToken
+import kr.hs.study.studybackendkotlin.exception.ExpiredJwtException
+import kr.hs.study.studybackendkotlin.repository.auth.RefreshTokenRepository
+import kr.hs.study.studybackendkotlin.service.auth.UserDetailsService
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JwtTokenProvider(
+    private val jwtProperties: JwtProperties,
+    private val authDetailsService: UserDetailsService,
+    private val refreshTokenRepository: RefreshTokenRepository
+) {
+    private val ACCESS_KEY: String = "access_token"
+    private val REFRESH_KEY: String = "refresh_token"
+
+    // 토큰 추가
+    fun createToken(userId: String, type: String, time: Long): String {
+        val now = Date()
+
+        return Jwts.builder().signWith(SignatureAlgorithm.HS256, jwtProperties.secretKey)
+            .setSubject(userId)
+            .setHeaderParam("type", type)
+            .setIssuedAt(now)
+            .setExpiration(Date(now.time + time))
+            .compact()
+    }
+
+    // access 토큰 추가
+    fun createAccessToken(userId: String): String
+        = createToken(userId, ACCESS_KEY, jwtProperties.accessTime)
+
+    // refresh 토큰 추가
+    fun createRefreshToken(userId: String): String {
+        val token: String = createToken(userId, REFRESH_KEY, jwtProperties.refreshTime)
+        refreshTokenRepository.save(RefreshToken(token, userId))
+        return token
+    }
+
+    // 토큰 인코딩
+    fun parseToken(bearerToken: String?): String? {
+        val prefix = jwtProperties.prefix + " "
+        if (bearerToken != null && bearerToken.startsWith(prefix)) {
+            return bearerToken.replace(prefix, "")
+        }
+        return null
+    }
+
+    // 토큰 디코딩
+    fun resolveToken(request: HttpServletRequest): String? {
+        val bearer: String = request.getHeader(jwtProperties.header)
+        return parseToken(bearer)
+    }
+
+    // 사용자 권한 (인가)
+    fun authorization(token: String): UsernamePasswordAuthenticationToken {
+        val userDetails: UserDetails = authDetailsService.loadUserByUsername(getTokenSubject(token))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+
+    // 1. userId을 받기 위해 parser
+    fun getTokenBody(token: String): Claims {
+        try {
+            return Jwts.parser().setSigningKey(jwtProperties.secretKey)
+                .parseClaimsJwt(token).body
+        } catch (e: Exception) {
+            throw ExpiredJwtException()
+        }
+    }
+
+    // 2. userId 받기
+    fun getTokenSubject(subject: String): String
+        = getTokenBody(subject).subject
+}
