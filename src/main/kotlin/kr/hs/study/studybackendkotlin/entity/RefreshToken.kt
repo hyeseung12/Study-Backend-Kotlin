f5c7ec468194e39ee94477c8450efbe507295926
MsEmily1020 <peace10200khs@gmail.com>
2024-07-21 20:24:50 +0900

    fix: @Id import 수정

--- src/main/kotlin/kr/hs/study/studybackendkotlin/entity/RefreshToken.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/entity/RefreshToken.kt
@@ -1,6 +1,6 @@
 package kr.hs.study.studybackendkotlin.entity
 
-import jakarta.persistence.Id
+import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
 @RedisHash(value = "refreshToken", timeToLive = 2592000L)
