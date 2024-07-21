ccaaf1f2882d9e02c8b3651a1a45231cddb1e6ff
MsEmily1020 <peace10200khs@gmail.com>
2024-07-21 19:25:15 +0900

    add: ErrorCode 추가

---
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/exception/error/ErrorCode.kt
@@ -0,0 +1,14 @@
+package kr.hs.study.studybackendkotlin.exception.error
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode(
+    val status: HttpStatus,
+    val message: String
+) {
+    USER_INCORRECT(HttpStatus.BAD_REQUEST, "Check your email/id or password"),
+
+    USER_DUPLICATE(HttpStatus.CONFLICT, "아이디 또는 이메일이 중복되었습니다."),
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+}
