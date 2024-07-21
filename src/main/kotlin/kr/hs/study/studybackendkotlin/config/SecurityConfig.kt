5bcfde3c3aed885474e2e5ec34dccc2b101c90e0
MsEmily1020 <peace10200khs@gmail.com>
2024-07-21 20:37:11 +0900

    fix: Security 수정

--- src/main/kotlin/kr/hs/study/studybackendkotlin/config/SecurityConfig.kt
+++ src/main/kotlin/kr/hs/study/studybackendkotlin/config/SecurityConfig.kt
@@ -2,13 +2,15 @@ package kr.hs.study.studybackendkotlin.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 
 @EnableWebSecurity
 @Configuration
@@ -21,10 +23,19 @@ class SecurityConfig {
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             csrf { disable() }
-            cors { disable() }
             formLogin { disable() }
             headers { disable() }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+            authorizeRequests {
+                authorize(anyRequest, permitAll)
+            }
+            exceptionHandling {
+                authenticationEntryPoint = HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+            }
         }
+
         return http.build()
     }
 }
